02fcd9270fa17f0a7d092de70f23b9fae0f51589
Pradip Sarkar <sarkar.pradip2002@gmailcom>
2022-03-24 23:48:14 +0530

    Final Commit

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from crypt import methods
 from flask import Flask,request, render_template
 import os
+import numpy as np
 from keras.preprocessing import image
+from keras.models import load_model
 
-import digit_recognition as dj
+model=load_model('Digit Recognisation')
 
 app = Flask(__name__)
 
 # model=pickle.load(open('model.pkl','rb'))
 # saver = tf.train.Saver(tf.global_variables(), write_version=tf.train.SaverDef.V1)
+def predict_digit(img):
+    # img_flattern=x_train.reshape(1,28*28)
+    predict=model.predict(img)
+    final_digit=np.argmax(predict)
+    return final_digit
 
 def prediction(img_path):
     i=image.load_img(img_path,target_size=(28,28))
@@ -16,7 +22,7 @@ def prediction(img_path):
     i=image.img_to_array(i)/255.0
     print(i.size)
     i=i.reshape(1,28,28)
-    p=dj.predict_digit(i)
+    p=predict_digit(i)
     return p
 
 @app.route('/')
